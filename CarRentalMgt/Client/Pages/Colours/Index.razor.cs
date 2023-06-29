5bd1ef4e1901b2c6adc771a4c360ad117c90ce4d
David RG <davidromerog@gmail.com>
2023-06-29 16:26:54 +0800

    Implemented in Colours, moving the business logic from the Index to a partial class and implementing the Global Error Handling for the Index page.

---
+++ CarRentalMgt/Client/Pages/Colours/Index.razor.cs
@@ -0,0 +1,46 @@
+﻿using CarRentalMgt.Client.Services;
+using CarRentalMgt.Client.Static;
+using CarRentalMgt.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http.Json;
+
+namespace CarRentalMgt.Client.Pages.Colours
+{
+    //DRG: Partial means this class is a partial implementation that works toghether with other class...
+    public partial class Index : IDisposable //DRG We added IDisposable for the Global Error Handling
+    {
+        //DRG: The Inject were copied from the razor file @inject transformed into [Inject] the AspNetCore.Components was needed for these.
+        [Inject] HttpClient _client { get; set; }
+        [Inject] IJSRuntime js { get; set; }
+        [Inject] HttpInterceptorService _interceptor { get; set; } //DRG Global Error Handling
+
+        //DRG: The code is being moved also from the razor file into this class.
+        private List<Colour>? Colours;
+
+        protected async override Task OnInitializedAsync()
+        {
+            _interceptor.MonitorEvent(); //DRG Global Error Handling
+            //DRG To see the console message in the Browser rightclick inspect, go to the Console tab and you will see...
+            //Console.WriteLine("Hitting Code Behind");
+            Colours = await _client.GetFromJsonAsync<List<Colour>>($"{Endpoints.ColoursEndpoint}");
+        }
+
+        async Task Delete(int colourId)
+        {
+            var colour = Colours.First(q => q.Id == colourId);
+            var confirm = await js.InvokeAsync<bool>("confirm", $"Do you want to delete {colour.Name}?");
+            if (confirm)
+            {
+                _interceptor.MonitorEvent(); //DRG Global Error Handling
+                await _client.DeleteAsync($"{Endpoints.ColoursEndpoint}/{colourId}");
+                await OnInitializedAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            _interceptor.DisposeEvent();
+        }
+    }
+}
--- CarRentalMgt/Client/Program.cs
+++ CarRentalMgt/Client/Program.cs
@@ -1,18 +1,31 @@
 using CarRentalMgt.Client;
+using CarRentalMgt.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 //DRG: This line is the setup of the HttpClient that will be used in Pages/FetchData.razor as @inject HttpClient Http
 //     and is configured to look at the ServerAPI on its address, to look at the API automatically from the razor page.
-builder.Services.AddHttpClient("CarRentalMgt.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+//builder.Services.AddHttpClient("CarRentalMgt.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+//    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+//DRG For the Global Error Handling we modified the line......
+builder.Services.AddHttpClient("CarRentalMgt.ServerAPI", (sp, client) => 
+    { 
+        client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+        client.EnableIntercept(sp);
+    }).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("CarRentalMgt.ServerAPI"));
+
+//DRG For Global Error Handling...
+builder.Services.AddHttpClientInterceptor();
+builder.Services.AddScoped<HttpInterceptorService>();
+//End DRG.....
 
 builder.Services.AddApiAuthorization();
 
