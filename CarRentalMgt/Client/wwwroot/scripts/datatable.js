da3f2084533cbbfaa7b8aa2c961b3410e525442a
David RG <davidromerog@gmail.com>
2023-07-15 18:56:06 +0800

    Changing the ASPNet version to 7.0.9 and start adding functions to change the table style on the pages

---
+++ CarRentalMgt/Client/wwwroot/scripts/datatable.js
@@ -0,0 +1,15 @@
+﻿//DRG Script to be used in the Index.HTML file to add or dispose the DataTable
+//    and will be used in the Index.razor from the different pages Bookings, etc...
+function AddDataTable(table) {
+    $(document).ready(function () {
+        $(table).DataTable();
+    })
+}
+
+function DataTablesDispose(table) {
+    $(document).ready(function () {
+        $(table).DataTable().destroy();
+        var element = document.querySelector(table + '_wrapper');
+        element.parentNode.removeChild(element);
+    })
+}
